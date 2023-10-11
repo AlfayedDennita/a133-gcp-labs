56a89ac0ed8ea15e7394fc156c298e1088df3bf2
Alfayed Dennita <alfayeddennita@gmail.com>
2023-10-11 16:50:16 +0700

    Config

--- routes/record.js
+++ routes/record.js
@@ -11,10 +11,10 @@ const multer = Multer({
 
 // TODO: Sesuaikan konfigurasi database
 const connection = mysql.createConnection({
-    host: 'public_ip_sql_instance_Anda',
+    host: '34.101.243.189',
     user: 'root',
-    database: 'nama_database_Anda',
-    password: 'password_sql_Anda'
+    database: 'money_tracker_app',
+    password: '4)n.clU)*|b>|utx'
 })
 
 router.get("/dashboard", (req, res) => {
